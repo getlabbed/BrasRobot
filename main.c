023d954f8ebe5a8397f67b86ccfed64de4d2940e
Dyke10 <Dyke10.yt@gmail.com>
2021-11-22 11:31:09 -0500

    test

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "Seriel.h"
+
+#define NB_CARACTERE_MAX 10
+
+// https://www.xanthium.in/Serial-Port-Programming-using-Win32-API
+
+int main() {
+    DWORD typeEvenement = 0; // Variable qui permet de connaître le type d'évènement (lorsque celui-ci survient)
+    char sChaine[NB_CARACTERE_MAX] = {0}; // Mémoire tampon qui enregistrera les données reçues sur le port sériel
+
+    return 0;
+}
