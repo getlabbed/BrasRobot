4f9f07d2ad90cb96b496a8993bbbe7e2eb58e8b5
Yanick <1927941@cegepoutaouais.qc.ca>
2021-11-22 13:18:12 -0600

    Clion Fix

---
+++ cmake-build-debug/CMakeFiles/testtt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testtt.dir/main.c.obj"
+  "testtt.exe"
+  "testtt.exe.manifest"
+  "testtt.lib"
+  "testtt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testtt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,12 +1,6 @@
-#include "Seriel.h"
-
-#define NB_CARACTERE_MAX 10
-
-// https://www.xanthium.in/Serial-Port-Programming-using-Win32-API
+#include <stdio.h>
 
 int main() {
-    DWORD typeEvenement = 1; // Variable qui permet de connaître le type d'évènement (lorsque celui-ci survient)
-    char sChaine[NB_CARACTERE_MAX] = {0}; // Mémoire tampon qui enregistrera les données reçues sur le port sériel
-    //Test de push Alexis Valiquette
+    printf("Hello, World!\n");
     return 0;
 }
