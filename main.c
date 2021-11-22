67971a16e2789e246c477497146e39af5e4fd0c8
Dyke10 <1927941@cegepoutaouais.qc.ca>
2021-11-22 11:39:06 -0500

    nombre 1

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 // https://www.xanthium.in/Serial-Port-Programming-using-Win32-API
 
 int main() {
-    DWORD typeEvenement = 0; // Variable qui permet de connaître le type d'évènement (lorsque celui-ci survient)
+    DWORD typeEvenement = 1; // Variable qui permet de connaître le type d'évènement (lorsque celui-ci survient)
     char sChaine[NB_CARACTERE_MAX] = {0}; // Mémoire tampon qui enregistrera les données reçues sur le port sériel
 
     return 0;
